3e8872e2215c78e33afd24821d653248ba8c52a2
ali <eali7563@gmail.com>
2025-12-20 22:30:13 +0200

    update API routes to restrict category access to index only and adjust controller namespace

--- Modules/Category/routes/api.php
+++ Modules/Category/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Category\Http\Controllers\CategoryController;
+use Modules\Category\Http\Controllers\Api\CategoryController;
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('categories', CategoryController::class)->names('category');
+Route::middleware([])->prefix('v1')->group(function () {
+    Route::apiResource('categories', CategoryController::class)
+        ->only('index')
+        ->names('category');
 });
