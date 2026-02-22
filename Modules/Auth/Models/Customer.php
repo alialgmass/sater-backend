f26a1a01574e507ca19cacd836dbaa7d75d4636a
ali <eali7563@gmail.com>
2026-02-22 22:49:22 +0200

    feat: Implement a new authentication and customer management system with OTP verification, alongside core cart, wishlist, order, and checkout functionalities.

--- Modules/Auth/Models/Customer.php
+++ Modules/Auth/Models/Customer.php
@@ -2,43 +2,7 @@
 
 namespace Modules\Auth\Models;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-
-class Customer extends Authenticatable
+class Customer
 {
-    use HasFactory, Notifiable, HasApiTokens, SoftDeletes;
 
-    protected $guarded = [];
-
-    protected $hidden = [
-        'password',
-    ];
-
-    protected $casts = [
-        'password' => 'hashed',
-    ];
-
-    public function profile()
-    {
-        return $this->hasOne(\Modules\Customer\Models\CustomerProfile::class);
-    }
-
-    public function addresses()
-    {
-        return $this->hasMany(\Modules\Customer\Models\CustomerAddress::class);
-    }
-
-    public function privacySettings()
-    {
-        return $this->hasOne(\Modules\Customer\Models\ProfilePrivacySetting::class);
-    }
-
-    public function orders()
-    {
-        return $this->hasMany(\Modules\Order\Models\Order::class);
-    }
 }
---
+++ Modules/Customer/Models/Customer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Modules\Auth\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Propaganistas\LaravelPhone\Casts\RawPhoneNumberCast;
+
+class Customer extends Authenticatable
+{
+    use HasFactory, Notifiable, HasApiTokens, SoftDeletes;
+
+    protected $guarded = [];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    protected $casts = [
+        'password' => 'hashed',
+        'phone' => RawPhoneNumberCast::class.'EG'
+    ];
+
+    public function profile()
+    {
+        return $this->hasOne(\Modules\Customer\Models\CustomerProfile::class);
+    }
+
+    public function addresses()
+    {
+        return $this->hasMany(\Modules\Customer\Models\CustomerAddress::class);
+    }
+
+    public function privacySettings()
+    {
+        return $this->hasOne(\Modules\Customer\Models\ProfilePrivacySetting::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(\Modules\Order\Models\Order::class);
+    }
+}
